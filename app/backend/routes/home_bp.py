4bdb201b7be39a89193c06dfbe762d906c82027c
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-06-15 20:11:01 -0300

    add files

--- app/backend/routes/home_bp.py
+++ app/backend/routes/home_bp.py
@@ -6,3 +6,10 @@ home_bp = Blueprint('home', __name__,template_folder='../../templates')
 @home_bp.route('/home', methods=["GET"])
 def return_home():
     return render_template('home.html')
+
+
+@home_bp.route('/nofify', methods=["GET"])
+def return_notify():
+    return render_template('notify.html')
+
+

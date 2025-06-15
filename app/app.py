6744800f5a24352faf35581ef0dd31c49236866e
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-06-15 20:01:19 -0300

    add files

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, Flask
 from backend.routes.login_register_bp import login_register_bp
+from backend.routes.home_bp import home_bp
 
 def CreateApp():
     app = Flask(__name__)
     app.register_blueprint(login_register_bp)
+    app.register_blueprint(home_bp)
     return app
 
 app = CreateApp()

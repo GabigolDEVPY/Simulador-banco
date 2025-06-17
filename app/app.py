a8190831b84637b1a64d314168b36d5857147a1d
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-06-17 19:15:16 -0300

    add files

--- app/app.py
+++ app/app.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, Flask
 from backend.routes.login_register_bp import login_register_bp
 from backend.routes.home_bp import home_bp
+from backend.routes.transfer import transfer_bp
+import sys
+sys.dont_write_bytecode = True
+
 
 def CreateApp():
     app = Flask(__name__)
     app.register_blueprint(login_register_bp)
     app.register_blueprint(home_bp)
+    app.register_blueprint(transfer_bp)
     return app
 
 app = CreateApp()
--- app/backend/routes/transfer.py
+++ app/backend/routes/transfer.py
@@ -5,3 +5,11 @@ transfer_bp = Blueprint('transfer', __name__,template_folder='../../templates/tr
 @transfer_bp.route('/transfer', methods=['GET'])
 def return_transfer_page():
     return render_template('transfer.html')
+
+@transfer_bp.route('/conferir-transfer', methods=['POST'])
+def return_transfer_confer():
+    return render_template('transfer-verify.html')
+
+@transfer_bp.route('/result-transfer', methods=['POST'])
+def return_result_transfer():
+    return render_template('result-transfer.html')

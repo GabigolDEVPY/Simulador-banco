87fff244eb7d436bfed36caf19fb6f94e271fdea
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-06-17 18:47:10 -0300

    add files

--- app/backend/routes/transfer.py
+++ app/backend/routes/transfer.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template
 
-transfer_bp = Blueprint('transfer', __name__,template_folder='../../templates')
+transfer_bp = Blueprint('transfer', __name__,template_folder='../../templates/transfer')
+
+@transfer_bp.route('/transfer', methods=['GET'])
+def return_transfer_page():
+    return render_template('transfer.html')

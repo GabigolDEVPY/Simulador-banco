f944410fa355097c1cdb27632bbb973a393655e2
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-06-19 10:38:07 -0300

    add files

--- app/backend/routes/pig.py
+++ app/backend/routes/pig.py
@@ -4,9 +4,9 @@ from flask import Blueprint, render_template, request
 pig_bp = Blueprint('pig', __name__,template_folder='../../templates/pig')
 
 caixinhas = [
-        {"id": 1, "nome": "Viagem", "valor": 1300, "imagem": "../static/imgs-caixinha/viagem.jpeg", "valor_investido": 1200, "meta": 20000, "ganhos": 254},        
-        {"id": 5, "nome": "Avião", "valor": 10000, "imagem": "../static/imgs-caixinha/carro.jpg", "valor_investido": 3000, "meta": 23644, "ganhos": 212},        
-        {"id": 9, "nome": "Carro", "valor": 2500, "imagem": "../static/imgs-caixinha/videogame.jpg", "valor_investido": 5000, "meta": 50078, "ganhos": 234}             
+        {"id": 1, "nome": "Viagem", "valor": 1300, "imagem": "/static/imgs-caixinha/viagem.jpeg", "valor_investido": 1200, "meta": 20000, "ganhos": 254},        
+        {"id": 5, "nome": "Avião", "valor": 10000, "imagem": "/static/imgs-caixinha/carro.jpg", "valor_investido": 3000, "meta": 23644, "ganhos": 212},        
+        {"id": 9, "nome": "Carro", "valor": 2500, "imagem": "/static/imgs-caixinha/videogame.jpg", "valor_investido": 5000, "meta": 50078, "ganhos": 234}             
 ]
 
 @pig_bp.route('/pigs', methods=['GET'])
@@ -22,5 +22,17 @@ def return_pig_page(id):
         
 @pig_bp.route('/pig/criarpig', methods=["GET"])
 def return_pig_create():
-    return render_template('criarpig.html')        
-            
+    return render_template('criarpig.html')      
+
+@pig_bp.route('/pigs/pig/guardarpig/<int:id>', methods=["GET"])
+def return_guardar_pig(id):
+      print(id)
+      for caixinha in caixinhas:
+        if caixinha['id'] == id:
+            return render_template('guardarpig.html', caixinha=caixinha)          
+
+@pig_bp.route('/pigs/pig/deletepig/<int:id>', methods=["GET"])
+def delete_pig(id):
+    for caixinha in caixinhas:
+        if caixinha['id'] == id:
+            caixinhas.remove(caixinha)

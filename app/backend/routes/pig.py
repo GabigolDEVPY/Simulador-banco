172b14f78be98105159cc0ac04dfc4e2a7a57d27
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-06-18 21:13:56 -0300

    add files

--- app/backend/routes/pig.py
+++ app/backend/routes/pig.py
@@ -5,7 +5,7 @@ pig_bp = Blueprint('pig', __name__,template_folder='../../templates/pig')
 
 @pig_bp.route('/pigs', methods=['GET'])
 def return_pigs_page():
-    caixinhas = [{"nome": "Viagem", "valor": 1300, "imagem": "../static/imgs/viagens.webp"}]
+    caixinhas = [{"id": 1, "nome": "Viagem", "valor": 1300, "imagem": "../static/imgs/viagens.webp"}]
     return render_template('pigs.html', caixinhas=caixinhas, num_caixinhas= 4 - len(caixinhas) )
 
 

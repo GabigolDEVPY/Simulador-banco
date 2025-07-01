a75340dc9bf260edc0c50c94f1a19df8192f449b
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-07-01 20:30:35 -0300

    add files

--- app/backend/routes/transfer.py
+++ app/backend/routes/transfer.py
@@ -1,18 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from..utils.auth import login_required
+from..utils.payment import Payment
 
 transfer_bp = Blueprint('transfer', __name__,template_folder='../../templates/transfer')
 
 @transfer_bp.route('/transfer', methods=['GET'])
-
+@login_required
 def return_transfer_page():
     return render_template('transfer.html')
 
 @transfer_bp.route('/conferir-transfer', methods=['POST'])
-
+@login_required
 def return_transfer_confer():
+    dados = request.form.to_dict()
+    print(dados)
+
     return render_template('transfer-verify.html')
 
 @transfer_bp.route('/result-transfer', methods=['POST'])
-
+@login_required    
 def return_result_transfer():
     return render_template('result-transfer.html')

40ba4f8402d11e49fe1939cf8f74fad2bce480b8
Gabriel Rocha Dias <gabigolbr26@gmail.com>
2025-07-19 21:00:06 -0300

    add files

--- app/backend/utils/pig.py
+++ app/backend/utils/pig.py
@@ -13,3 +13,9 @@ class Pig:
     def criar_pig(dados):
         values = (session["user_id"], dados["meta"], 0, dados["imagem"], dados["nome"])
         BD_execute.execute_comand("INSERT INTO pigs (user_id, meta_pig, total_bruto, image_pig, nome_pig) VALUES (%s, %s, %s, %s, %s)", *values)
+
+    @staticmethod
+    def guardar_pig(valor, pig_id):
+        comand = ("UPDATE pigs SET total_bruto = total_bruto + %s WHERE pig_id = %s AND user_id = %s")    
+        values = (valor, pig_id, session['user_id'])
+        BD_execute.execute_comand(comand, *values)
